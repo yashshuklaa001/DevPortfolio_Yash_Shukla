c3918b1c5a2c76e552bb02d735a6afa81fef33cd
Yash Shukla <yashshuklaa001@gmail.com>
2025-07-31 02:38:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
-import AboutSection from './components/homepage/about';
+'use client';
 
-import ContactSection from './components/homepage/contact';
-import Education from './components/homepage/education';
-import Experience from './components/homepage/experience';
-import HeroSection from './components/homepage/hero-section';
-import Projects from './components/homepage/projects';
-import Skills from './components/homepage/skills';
+import dynamic from 'next/dynamic';
 
-export default async function Home() {
+const HeroSection = dynamic(() => import('./components/homepage/hero-section'), { ssr: false });
+const AboutSection = dynamic(() => import('./components/homepage/about'), { ssr: false });
+const Education = dynamic(() => import('./components/homepage/education'), { ssr: false });
+const Skills = dynamic(() => import('./components/homepage/skills'), { ssr: false });
+const Projects = dynamic(() => import('./components/homepage/projects'), { ssr: false });
+const ContactSection = dynamic(() => import('./components/homepage/contact'), { ssr: false });
+// const Experience = dynamic(() => import('./components/homepage/experience'), { ssr: false });
+
+export default function Home() {
   return (
     <>
       <HeroSection />
@@ -16,7 +19,6 @@ export default async function Home() {
       {/* <Experience /> */}
       <Skills />
       <Projects />
-
       <ContactSection />
     </>
   );
